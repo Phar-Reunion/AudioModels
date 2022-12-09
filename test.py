3deaa4a02d07f1e2828d1966760b8e3ce598fb28
mattis - Helife <mattis.dalleau@epitech.eu>
2022-12-09 11:11:29 +0400

    First Rock/Pop recognition

--- test.py
+++ test.py
@@ -1,137 +1,49 @@
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-import librosa as rosa
+import librosa
+import tensorflow
+import numpy
 
-class MusicAnalyzerException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-
-class MusicAnalyzer:
-
+class MusicAnalyzer(tensorflow.keras.Model):
     def __init__(self):
-        self.dataset = {}
-        self.model = None
+        super(MusicAnalyzer, self).__init__()
+        self.conv1 = tensorflow.keras.layers.Conv2D(32, 3, activation='relu')
+        self.flatten = tensorflow.keras.layers.Flatten()
+        self.d1 = tensorflow.keras.layers.Dense(128, activation='relu')
+        self.d2 = tensorflow.keras.layers.Dense(10, activation='softmax')
+        self.valid_extensions = ['.ogg', '.wav']
 
-    def add_genre(self, genre):
-        if self.dataset.get(genre) is None:
-            self.dataset[genre] = []
-        return self
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        return self.d2(x)
 
-    def add_melspectogram(self, genre, specto):
-        if self.dataset.get(genre) is None:
-            self.add_genre(genre)
-        self.dataset[genre].append(specto)
-        return self
+def load_mfcc(path, n_seconds=-1):
+    y, sr = librosa.load(path)
+    if n_seconds > 0:
+        y = y[:sr * n_seconds]
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    return mfcc, sr
 
-    def add_melspectograms(self, genre, spectos):
-        if self.dataset.get(genre) is None:
-            self.add_genre(genre)
-        self.dataset[genre].extend(spectos)
-        return self
+def load_mfcc_and_reshape(path, n_seconds=-1):
+    mfcc, sr = load_mfcc(path, n_seconds)
+    mfcc = numpy.reshape(mfcc, (1, mfcc.shape[0], mfcc.shape[1], 1))
+    return mfcc
 
-    def build_model(self, layers=[]):
-        self.model = tf.keras.models.Sequential(layers)
-        return self
+if __name__ == '__main__':
+    import os
 
-    def add_layer(self, layer):
-        self.model.add(layer)
-        return self
+    ma = MusicAnalyzer()
+    ma.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    def compile_model(self,
-            loss='categorical_crossentropy',
-            metrics=['accuracy'],
-            loss_weights=None,
-            optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
-            weighted_metrics=None,
-            run_eagerly=None,
-            steps_per_execution=None,
-            jit_compile=None,
-            **kwargs):
+    genres = ['rock', 'pop']
 
-        self.model.compile(
-            loss=loss,
-            metrics=metrics,
-            loss_weights=loss_weights,
-            optimizer=optimizer,
-            weighted_metrics=weighted_metrics,
-            run_eagerly=run_eagerly,
-            steps_per_execution=steps_per_execution,
-            jit_compile=jit_compile,
-            **kwargs)
-        return self
-
-    def train(self, epochs=1000, steps_per_epoch=100, validation_split=0.2):
-        if self.model is None:
-            raise MusicAnalyzerException("Model is not built yet")
-        if len(self.dataset) == 0:
-            raise MusicAnalyzerException("Dataset is empty")
-        for genre in self.dataset:
-            self.dataset[genre] = np.array(self.dataset[genre])
-        print(self.dataset)
-        self.model.fit(
-            self.dataset,
-            epochs=epochs,
-            steps_per_epoch=steps_per_epoch,
-            validation_split=validation_split)
-        return self
-
-    def save(self, path):
-        if self.model is None:
-            raise MusicAnalyzerException("Model is not built yet")
-        self.model.save(path)
-        return self
-
-def load_music_and_extract_random_samples(path, sample_count=10, sample_length=1000):
-    samples = []
-    music, sr = rosa.load(path)
-
-    for i in range(sample_count):
-        start = np.random.randint(0, len(music) - sample_length)
-        samples.append(music[start:start+sample_length])
-    return samples
-
-def music_samples_to_melspectrogram(samples):
-    melspectrogram = []
-    for sample in samples:
-        melspectrogram.append(rosa.feature.melspectrogram(sample))
-    return melspectrogram
-
-def load_musics_and_extract_random_samples(paths, sample_count=10, sample_length=1000):
-    musics = []
-    for path in paths:
-        musics.extend(load_music_and_extract_random_samples(path, sample_count, sample_length))
-    return musics
-
-msa = MusicAnalyzer()
-msa.add_genre("rock").add_melspectograms("rock",
-        music_samples_to_melspectrogram(
-            load_musics_and_extract_random_samples([
-                "rock/rock1.ogg",
-                "rock/rock2.ogg"
-            ])
-        )
-    )
-"""
-msa.add_genre("pop").add_musics("pop",
-        music_samples_to_melspectrogram(
-            load_musics_and_extract_random_samples([
-                "pop/pop1.ogg",
-                "pop/pop2.ogg"
-            ])
-        ))
-"""
-
-msa.build_model([
-        #tf.keras.layers.Flatten(input_shape=(128, 128)),
-        #tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(2)
-    ])
-msa.compile_model()
-msa.train()
-msa.save("model.h5")
-
-msa.model.predict(np.array([
-        rosa.feature.melspectrogram(rosa.load("rock/rock2.ogg")[0]),
-        rosa.feature.melspectrogram(rosa.load("pop/pop2.ogg")[0]),
-    ]))
+    for i, genre in enumerate(genres):
+        genresnp = numpy.array([0] * len(genres))
+        genresnp[i] = 1
+        for file in os.listdir(genre):
+            for ext in ma.valid_extensions:
+                if file.endswith(ext):
+                    mfcc = load_mfcc_and_reshape(os.path.join(genre, file), 30)
+                    ma.fit(mfcc, numpy.array([i]), epochs=1)
+    print(ma.predict(load_mfcc_and_reshape('rock/rock1.ogg', 30)))
+    print(ma.predict(load_mfcc_and_reshape('pop/pop2.ogg', 30)))
