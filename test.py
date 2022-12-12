f36f51c1cc02b8959c4dfda7507d7885c2701a03
Alexis Picard <69953258+NotaSurgele@users.noreply.github.com>
2022-12-12 15:18:21 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,7 @@
 import librosa
 import tensorflow
 import numpy
-
-class MusicAnalyzer(tensorflow.keras.Sequential):
-    def __init__(self, nclasses, input_shape=()):
-        super(MusicAnalyzer, self).__init__()
-
-        self.add(tensorflow.keras.layers.Conv2D(32, (3, 3), activation='relu'))#, input_shape=input_shape))
-        self.add(tensorflow.keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
-        self.add(tensorflow.keras.layers.BatchNormalization())
-
-        self.add(tensorflow.keras.layers.Conv2D(32, (3, 3), activation='relu'))
-        self.add(tensorflow.keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
-        self.add(tensorflow.keras.layers.BatchNormalization())
-
-        self.add(tensorflow.keras.layers.Conv2D(32, (2, 2), activation='relu'))
-        self.add(tensorflow.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), padding='same'))
-        self.add(tensorflow.keras.layers.BatchNormalization())
-
-        self.add(tensorflow.keras.layers.Flatten())
-        self.add(tensorflow.keras.layers.Dense(64, activation='relu'))
-        self.add(tensorflow.keras.layers.Dropout(0.3))
-
-        self.add(tensorflow.keras.layers.Dense(nclasses, activation='softmax'))
-
-        self.valid_extensions = ['.ogg', '.wav']
+from sklearn.model_selection import train_test_split
 
 def load_mfccs_and_reshape(path, n_seconds):
     mfccs = []
@@ -40,20 +17,41 @@ def load_mfccs_and_reshape(path, n_seconds):
 if __name__ == '__main__':
     import os
 
-    genres = ['disco', 'jazz', 'rock', 'pop', 'blues', 'reggae', 'hiphop', 'metal', 'classical', 'country']
-    sec = 30
-    epochs = 30
+    genres = ['classic', 'rock', 'pop', None]
+    sec = 10
+    epochs = 120
 
     print(genres)
-
     def train_model(sec, epochs, genres):
-        ma = MusicAnalyzer(len(genres))
+        ma = tensorflow.keras.Sequential()
+
+        #ma.add(tensorflow.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+        ma.add(tensorflow.keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+        ma.add(tensorflow.keras.layers.BatchNormalization())
+
+        ma.add(tensorflow.keras.layers.Conv2D(32, (3, 3), activation='relu'))
+        ma.add(tensorflow.keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+        ma.add(tensorflow.keras.layers.BatchNormalization())
+
+        ma.add(tensorflow.keras.layers.Conv2D(32, (2, 2), activation='relu'))
+        ma.add(tensorflow.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), padding='same'))
+        ma.add(tensorflow.keras.layers.BatchNormalization())
+
+        ma.add(tensorflow.keras.layers.Flatten())
+        ma.add(tensorflow.keras.layers.Dense(64, activation='relu'))
+        ma.add(tensorflow.keras.layers.Dropout(0.3))
+
+        for i in range(5):
+            ma.add(tensorflow.keras.layers.Dense(len(genres), activation='softmax'))
+
         ma.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
         for i, genre in enumerate(genres):
+            if genre is None:
+                continue
             print("Genre: " + genre)
             for file in os.listdir(genre):
-                for ext in ma.valid_extensions:
+                for ext in ['.ogg', '.wav']:
                     if file.endswith(ext):
                         print(f'Loading {file}...')
                         mfccs = load_mfccs_and_reshape(os.path.join(genre, file), sec)
@@ -61,7 +59,8 @@ if __name__ == '__main__':
                         for mfcc in mfccs:
                             ma.fit(mfcc, numpy.array([i]), epochs=epochs)
                         break
-        ma.save('model2.h5')
+        ma.save('model.h5')
+        return ma
 
     def predict(ma, files, sec):
         def print_genre(result, genres):
@@ -77,5 +76,7 @@ if __name__ == '__main__':
                 print(f'File: {song} part {i}')
                 print_genre(result, genres)
                 print("====================================")
+
+    result = train_model(sec, epochs, genres)
     print(result.summary())
-    predict(result, ['./touhou.ogg'], sec)
+    predict(result, ['./acdc.wav'], sec)
